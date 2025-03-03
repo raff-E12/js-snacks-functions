0a5c8fd171596ae05c772ac21d7301b1961d5d93
Raff_12 <raffelecoppola381@gmail.com>
2025-03-03 18:02:59 +0100

    feat: Consegna del quarto esercizio

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,20 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+const Vocals_Fun = (words)=>{
+    let result = '';
+    let vocals = ['a', 'e', 'i', 'o', 'u'];
+    let vocals_number = 0;
+    for (let index = 0; index < words.length; index++) {
+        if (vocals.indexOf(words[index]) !== -1) {
+            vocals_number++;
+        } 
+    }
+   return result = 'Comprendono ' + vocals_number + ' vocali in tutto.';
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(Vocals_Fun(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
