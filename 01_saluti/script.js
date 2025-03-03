3cd6f492dbe9e0c45493f9eb93ffbd522e29b884
Raff_12 <raffelecoppola381@gmail.com>
2025-03-03 16:50:33 +0100

    feat: Risoluzione del primo esercizio

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -5,9 +5,18 @@ const userName = 'Mario';
 
 // Dichiara la funzione qui.
 
+function Username_con(user) {
+    return console.log('Ciao', user);
+}
+
+Username_con(userName);
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const User_Hello = (user)=>{
+   return console.log('Ciao', user);
+}
 
+User_Hello(userName);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
