8e2c178bcb1e27d58cf8906bada9ee0586842e13
Raff_12 <raffelecoppola381@gmail.com>
2025-03-03 17:05:35 +0100

    feat: Pubblicazione del secondo esercizio

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function List_Append() {
+    let name_list = [];
+    for (let index = 0; index < names.length; index++) {
+        name_list.push(names[index].slice(0, 1));
+    }
+    console.log(name_list);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+List_Append()
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
