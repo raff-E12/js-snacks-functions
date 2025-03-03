690e69ee6d134fada013bac304dc6f2a82a564ba
Raff_12 <raffelecoppola381@gmail.com>
2025-03-03 17:50:58 +0100

    feat: Modifica del terzo esercizio

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,18 +5,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-function Name_Append() {
+function Name_Append(list, letter) {
     let list_letter_primary = [];
-    for (let index = 0; index < names.length; index++) {
-        if (!names[index].indexOf('A')) {
-            list_letter_primary.push(names[index]);
+    for (let index = 0; index < list.length; index++) {
+        if (list[index].indexOf(letter) === 0) {
+            list_letter_primary.push(list[index]);
         }
     }
-    console.log(list_letter_primary);
+    return list_letter_primary;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-Name_Append()
+console.log(Name_Append(names, 'A'));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
